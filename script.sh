53ac25ec6d2fcfecbdab3bfbd3fef5967be96013
Haroun-Azoulay <ha.azoulay93@gmail.com>
2025-10-01 15:51:33 +0200

    [✨] Added frontend & Updated script.sh.

--- script.sh
+++ script.sh
@@ -1,34 +1,52 @@
 #!/bin/bash
 
 echo -e "Welcome to the Papernest test! \n"
-if [[ $1 == "run" ]]; then
+if [[ $1 == "back" ]]; then
 
-    echo "[WAIT -------------------------- Building your Docker image from Dockerfile...]"
+    echo "[WAIT -------------------------- Building your Docker backend image from Dockerfile...]"
     docker build -t paper .
 
     echo "[INFO -------------------------- Launching your container on port 8000]"
     docker run -p 8000:8000 -it paper
+elif [[ $1 == "front" ]]; then
+
+    echo "[WAIT -------------------------- Building your Docker frontend image from Dockerfile...]"
+    cd ./front
+    docker build -t paper-front .
+
+    echo "[INFO -------------------------- Launching your container on port 80]"
+    docker run -p 8080:80 -it paper-front
 fi
 launchApp() {
-    echo -e "What do you want to do? Type 'run' to launch the backend or 'help' to see instructions or 'exit' to leave the script: \n"
+    echo -e "What do you want to do? Type 'run back' to launch the backend, 'run front' to launch the frontend or 'help' to see instructions or 'exit' to leave the script: \n"
 
     read -r response
     response=$(echo "$response")
 
-    if [[ "$response" == "run" ]]; then
-        echo "[WAIT -------------------------- Building your Docker image from Dockerfile...]"
-
-        docker build -t paper .
+    if [[ "$response" == "run back" ]]; then
+        echo "[WAIT -------------------------- Building your Docker backend image from Dockerfile...]"
+        cd ./front
+        docker build -t paper-back .
 
         echo "[INFO -------------------------- Launching your container on port 8000]"
+        cd ./front
+        docker run -p 8000:8000 -it paper-back
 
-        docker run -p 8000:8000 -it paper
+    elif [[ "$response" == "run front" ]]; then
+        echo "[WAIT -------------------------- Building your Docker frontend image from Dockerfile...]"
+
+        docker build -t build paper-front .
+
+        echo "[INFO -------------------------- Launching your container on port 80]"
+
+        docker run -ti -p 8080:80 paper-front
 
     elif [[ "$response" == "help" ]]; then
         echo
         echo "=================== HELP ==================="
         echo "Available commands:"
-        echo "  run   -> Build the Docker image and run the backend container on port 8000"
+        echo "  run back   -> Build the Docker backend image and run the backend container on port 8000"
+        echo "  run frontend   -> Build the Docker frontend image and run the backend container on port 8080"
         echo "  help  -> Display this help message"
         echo "  exit  -> Quit the script"
         echo
